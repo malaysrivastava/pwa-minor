caad5d2d941cfcfe80ec2b5624fb7675dd123a82
Malay Srivastava <malaypreet2013@gmail.com>
2022-03-23 11:06:18 +0530

    shaw-shaw2.0

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import View from './Footer-view'
 
 
 const Footer = () => {
+
+  const [show,setShow] = useState(true);
+  var excp = window.location.pathname;
+
+  useEffect(()=>{
+  console.log(excp)
+    if(excp=='/login'){
+      setShow(false);
+    }else{
+      setShow(true);
+    }
+  },[2])
+
+  if(show){
 return (
 	<div>
         <View/>
         </div>
 );
+} else{
+  return (<></>)
+}
 };
 export default Footer;
